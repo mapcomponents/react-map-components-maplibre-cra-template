087577e67008430f7fa5e81f3d0da9c9e5a21984
Marwin Ludwig <85504236+Padre1408@users.noreply.github.com>
2023-02-13 17:41:46 +0100

    Upgrade to React 18

--- template/src/index.js
+++ template/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { MapComponentsProvider } from "@mapcomponents/react-maplibre";
 
-ReactDOM.render(
-<React.StrictMode>
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+root.render(
+  <React.StrictMode>
     <MapComponentsProvider>
       <App />
     </MapComponentsProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
